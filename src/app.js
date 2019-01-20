e050bcfbdd6f4f3505f32011f6be995bbad5349e
Davide Brognoli <davide.brognoli@gmail.com>
2019-01-20 21:25:36 +0100

    My day

--- src/app.js
+++ src/app.js
@@ -1,1 +1,33 @@
 import './main.scss';
+import Chart from 'chart.js';
+
+import * as dataSource from '../build/data.json';
+
+const {my_day} = dataSource.default;
+const data = my_day.map(m => m.length);
+const backgroundColor = my_day.map(m => m.color);
+const labels = my_day.map(m => m.label);
+
+const ctx = document.getElementById('myChart');
+const config = {
+  type: 'doughnut',
+  data: {
+    datasets: [{
+      data,
+      backgroundColor
+    }],
+    labels
+  },
+  options: {
+    responsive: true,
+    legend: {
+      display: false
+    },
+    animation: {
+      animateScale: true,
+      animateRotate: true
+    }
+  }
+};
+
+new Chart(ctx, config);
